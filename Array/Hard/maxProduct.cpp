28c810917b84397c3a5856e445bba812b6689f6c
Aryan Prajapati <aryan.prajapati1713@gmail.com>
2026-03-03 22:42:19 +0530

    updated optimal of maxProd

--- Array/Hard/maxProduct.cpp
+++ Array/Hard/maxProduct.cpp
@@ -15,7 +15,7 @@ int maxProduct(vector<int> &arr){
     return maxProd;
 }
 
-// optimal: O(n)
+// optimal 1: O(n)
 int maxProductOptimal(vector<int> &arr){
     int prefixProd = 1, suffixProd = 1, maxProd = INT_MIN;
     for(int i=0; i<arr.size(); i++){
@@ -30,12 +30,27 @@ int maxProductOptimal(vector<int> &arr){
     return maxProd;
 }
 
+// optimal 2: O(n)
+int maxProductOptimal2(vector<int> &arr){
+    int maxProd = arr[0], minProd = arr[0], result = arr[0];
+    for(int i=1; i<arr.size(); i++){
+        if(arr[i] < 0)   swap(maxProd, minProd);
+
+        maxProd = max(arr[i], maxProd * arr[i]);
+        minProd = min(arr[i], minProd * arr[i]);
+
+        result = max(result, maxProd);
+    }
+    return result;
+}
+
 int main(){
     vector<int> arr;
     int ip;
     while(cin >> ip)    arr.emplace_back(ip);
 
     // cout << maxProduct(arr) << endl;
-    cout << maxProductOptimal(arr) << endl;
+    // cout << maxProductOptimal(arr) << endl;
+    cout << maxProductOptimal2(arr) << endl;
     return 0;
 }
